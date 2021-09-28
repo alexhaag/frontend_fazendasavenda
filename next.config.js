9f34789bd84dba027b43e5456ea1e07c70153851
Alexsandro <alexhaag@hotmail.com>
2021-09-28 15:48:01 -0300

    configuracao next.config

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  images: {
+    deviceSizes: [320, 640, 768, 1024, 1600],
+    domains: ['assets.vercel.com', 'firebasestorage.googleapis.com', 'storage.googleapis.com']
+  }
+}
